8b5d2025fca65a1d88c05679068d98058e55277d
Harold Martin <harold.martin@gmail.com>
2024-08-20 10:33:24 -0700

    Factor out CLI commands

---
+++ cli/src/main/java/me/haroldmartin/objective/cli/Utils.kt
@@ -0,0 +1,27 @@
+package me.haroldmartin.objective.cli
+
+internal const val HELP_MESSAGE = """
+    Usage: objective [-k OBJECTIVE_KEY] [COMMAND]
+    
+    Commands:
+        If no command is given, display the TUI
+        [o]bjects       List objects or pass an object ID to get a single object
+        [i]ndexes       List indexes or pass an index ID to get a single index
+        [s]earch        Pass an Index ID and a search term to query an index
+        co              Create an object from JSON file[s]
+"""
+
+internal fun parseArgsForObjectiveKey(args: Array<String>): String {
+    val keyIndex = args.indexOf("-k")
+    if (keyIndex < 0 || keyIndex >= args.lastIndex) {
+        throw NoObjectiveApiKeyError
+    }
+    return args[keyIndex + 1]
+}
+
+object NoObjectiveApiKeyError : Error(
+    "No Objective API key found in OBJECTIVE_KEY env var or -k flag",
+) {
+    @Suppress("UnusedPrivateMember")
+    private fun readResolve(): Any = NoObjectiveApiKeyError
+}
